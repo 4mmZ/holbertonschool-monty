b06156355293340b7a6d0ed1cd91f304eb568a37
vicenzoanza <viceanza2010@hotmail.com>
2023-05-16 08:36:41 -0700

    fix

--- aux_func.c
+++ aux_func.c
@@ -4,12 +4,12 @@
 void free_stack(stack_t **stack)
 {
 	stack_t *aux = *stack;
-	
+
 	if (*stack == NULL)
 	{
 	return;
 	}
-	while(*stack)
+	while (*stack)
 	{
 	aux = (*stack)->next;
 	free(*stack);
@@ -20,39 +20,42 @@ void free_stack(stack_t **stack)
 
 int is_int(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    if (s == NULL)
-        return (0);
+	if (s == NULL)
+	return (0);
 
-    if (s[0] == '-' || s[0] == '+')
-        i = 1;
+	if (s[0] == '-' || s[0] == '+')
+	i = 1;
 
-    for (; s[i] != '\0'; i++)
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-    }
+	for (; s[i] != '\0'; i++)
+	{
+	if (s[i] < '0' || s[i] > '9')
+	return (0);
+	}
 
-    return (1);
+	return (1);
 }
 
 char *trim(char *str)
 {
-    char *end;
+	char *end;
 
-    // Eliminar los espacios en blanco al principio
-    while (isspace((unsigned char)*str)) str++;
+	/* Eliminar los espacios en blanco al principio */
+	while (isspace((unsigned char)*str))
+	str++;
 
-    // Si la cadena está vacía, devolver una cadena vacía
-    if (*str == 0) return str;
+	/* Si la cadena está vacía, devolver una cadena vacía */
+	if (*str == 0)
+	return (str);
 
-    // Eliminar los espacios en blanco al final
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end)) end--;
+	/* Eliminar los espacios en blanco al final */
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end))
+		end--;
 
-    // Agregar el carácter nulo al final
-    *(end+1) = 0;
+	/* Agregar el carácter nulo al final */
+	*(end + 1) = 0;
 
-    return str;
+	return (str);
 }
