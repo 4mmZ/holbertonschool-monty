7ef6353bc50a7240b21f725fc9889ca5f681465d
4mmZ <andresmaxi111014@gmail.com>
2023-05-16 07:20:50 -0700

    fix: pop function stdeer exit status

--- functions.c
+++ functions.c
@@ -74,7 +74,7 @@ void pop_func(stack_t **stack, unsigned int line_number)
         if (*stack == NULL)
         {
                 fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-                return;
+                exit(EXIT_FAILURE);
         }
         
         stack_t *node_to_remove = *stack;
