35e833d87d890782e43daff08386383742b3ca88
vicenzoanza <viceanza2010@hotmail.com>
2023-05-16 09:00:44 -0700

    fix

--- functions.c
+++ functions.c
@@ -3,144 +3,143 @@
 
 void push_func(stack_t **stack, unsigned int line_number)
 {
-        char *arg = strtok(NULL, DELIM);
-        int n;
+	char *arg = strtok(NULL, DELIM);
+	int n;
 
-        if (!arg || !is_int(arg))
-        {
-                fprintf(stderr, "L%u: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+	if (!arg || !is_int(arg))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-        n = atoi(arg);
-        add_node(stack, n);
+	n = atoi(arg);
+	add_node(stack, n);
 }
 
 void pall_func(stack_t **stack, unsigned int line_number)
 {
-        stack_t *tmp = *stack;
+	stack_t *tmp = *stack;
 
-        (void)line_number;
+	(void)line_number;
 
-        while (tmp)
-        {
-                printf("%d\n", tmp->n);
-                tmp = tmp->next;
-        }
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }
 
 void add_node(stack_t **stack, int n)
 {
-        stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-        if (!new_node)
-        {
-                fprintf(stderr, "Error: malloc failed\n");
-                exit(EXIT_FAILURE);
-        }
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-        new_node->n = n;
-        new_node->prev = NULL;
-        new_node->next = *stack;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-        if (*stack)
-                (*stack)->prev = new_node;
+	if (*stack)
+		(*stack)->prev = new_node;
 
-        *stack = new_node;
+	*stack = new_node;
 }
 
 void pint_func(stack_t **stack, unsigned int line_number)
 {
-        stack_t *tmp = *stack;
+	stack_t *tmp = *stack;
 
-        (void)line_number;
+	(void)line_number;
 
-        if (tmp) 
-        {
-                printf("%d\n", tmp->n);
-        }
-        else
-        {
-                fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+	if (tmp)
+	{
+		printf("%d\n", tmp->n);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 }
 
 void pop_func(stack_t **stack, unsigned int line_number)
 {
-        (void)line_number;
+	(void)line_number;
 
-        if (*stack == NULL)
-        {
-                fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        
-        stack_t *node_to_remove = *stack;
-        
-        *stack = (*stack)->next;
-        if (*stack != NULL)
-                (*stack)->prev = NULL;
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	stack_t *node_to_remove = *stack;
 
-        free(node_to_remove);
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+
+	free(node_to_remove);
 }
 
 void swap_func(stack_t **stack, unsigned int line_number)
 {
-        (void)line_number;
+	(void)line_number;
 
-        if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-                
-        }
-        
-        stack_t *top = *stack;
-        stack_t *second = top->next;
-        
-        top->next = second->next;
-        if (second->next != NULL)
-                second->next->prev = top;
-                
-        second->prev = NULL;
-        second->next = top;
-        top->prev = second;
-        *stack = second;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *top = *stack;
+	stack_t *second = top->next;
+
+	top->next = second->next;
+	if (second->next != NULL)
+		second->next->prev = top;
+
+	second->prev = NULL;
+	second->next = top;
+	top->prev = second;
+	*stack = second;
 }
 
 void add_func(stack_t **stack, unsigned int line_number)
 {
 
-        (void)line_number;
-        
-        if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        
-        stack_t *top = *stack;
-        stack_t *second = top->next;
-        
-        int result = top->n + second->n;
-        
-        stack_t *new_node = malloc(sizeof(stack_t));
-        if (new_node == NULL)
-        {
-                fprintf(stderr, "Error: Failed to allocate memory\n");
-                exit(EXIT_FAILURE);
-        }
+	(void)line_number;
 
-        new_node->n = result;
-        new_node->prev = NULL;
-        new_node->next = second->next;
-        if (second->next != NULL)
-                second->next->prev = new_node;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-        free(top);
-        free(second);
+	stack_t *top = *stack;
+	stack_t *second = top->next;
 
-        *stack = new_node;
+	int result = top->n + second->n;
+
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: Failed to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = result;
+	new_node->prev = NULL;
+	new_node->next = second->next;
+	if (second->next != NULL)
+		second->next->prev = new_node;
+
+	free(top);
+	free(second);
+
+	*stack = new_node;
 }
--- interpreter.c
+++ interpreter.c
@@ -2,72 +2,73 @@
 
 int main(int argc, char **argv)
 {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    stack_t *stack = NULL;
-    unsigned int line_number = 0;
+FILE *file;
+char *line = NULL;
+size_t len = 0;
+ssize_t read;
+stack_t *stack = NULL;
+unsigned int line_number = 0;
 
-    // Definir la tabla de opcodes y funciones correspondientes
-    instruction_t instructions[] = {
-        {"push", push_func},
-        {"pall", pall_func},
-        {"pint", pint_func},
-        {"pop", pop_func},
-        {"swap", swap_func},
-        {"add", add_func},
-        {NULL, NULL}
-    };
+	/* Definir la tabla de opcodes y funciones correspondientes */
+	instruction_t instructions[] = {
+	{"push", push_func},
+	{"pall", pall_func},
+	{"pint", pint_func},
+	{"pop", pop_func},
+	{"swap", swap_func},
+	{"add", add_func},
+	{NULL, NULL}
+	};
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+	exit(EXIT_FAILURE);
+	}
 
-    while ((read = getline(&line, &len, file)) != -1)
-    {
-        line_number++;
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+	line_number++;
 
-        // Eliminar el caracter '\n' de la línea y eliminar los espacios en blanco al principio y al final
-        char *trimmed_line = trim(line);
+	/* Quitar caracter '\n' y quitar espacios en blanco al princ y final */
+	char *trimmed_line = trim(line);
 
-        // Skip empty lines
-        if (strlen(trimmed_line) == 0) {
-            continue;
-        }
+	/* Saltar linea vacia */
+	if (strlen(trimmed_line) == 0)
+	{
+		continue;
+	}
 
-        // Obtener el opcode y los argumentos de la línea
-        char *opcode = strtok(trimmed_line, " \n$");
+	/* Obtener el opcode y los argumentos de la línea */
+	char *opcode = strtok(trimmed_line, " \n$");
 
-        // Buscar el opcode en la tabla de opcodes y llamar a la función correspondiente
-        int i;
-        for (i = 0; instructions[i].opcode != NULL; i++)
-        {
-            if (strcmp(opcode, instructions[i].opcode) == 0)
-            {
-                instructions[i].f(&stack, line_number);
-                break;
-            }
-        }
-        if (instructions[i].opcode == NULL)
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
-    }
+	/* Buscar el opcode en la tabla de opcodes y llamar a la función */
+	int i;
 
-    // Liberar la memoria utilizada
-    fclose(file);
-    free_stack(&stack);
-    if (line)
-        free(line);
+	for (i = 0; instructions[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+		instructions[i].f(&stack, line_number);
+		break;
+		}
+	}
+	if (instructions[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
+	}
+	/* Liberar la memoria utilizada */
+	fclose(file);
+	free_stack(&stack);
+	if (line)
+	free(line);
 }
